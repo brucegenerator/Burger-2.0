1dea39e1f1031042e4536441a032ec2184dbc8df
Jonathan Roth <rothje@vcu.edu>
2020-06-24 11:16:42 -0400

    added more containers to handle separation of concerns and modal and sidedrawer

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    require: true
+                },
+                valid: false
             },
             street: {
                 elementType: 'input',
@@ -22,7 +26,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    require: true
+                },
+                valid: false
             },
             zipCode: {
                 elementType: 'input',
@@ -30,7 +38,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Zip Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    require: true
+                },
+                valid: false
             },
             country: {
                 elementType: 'input',
@@ -38,7 +50,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    require: true
+                },
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -46,7 +62,11 @@ class ContactData extends Component {
                     type: 'email',
                     placeholder: 'Your email'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    require: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -67,10 +87,14 @@ class ContactData extends Component {
         this.setState({
             loading: true
         });
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            
+            orderData: formData
         }
         axios.post('/orders.json', order)
             .then(response =>{
@@ -86,13 +110,54 @@ class ContactData extends Component {
             })
     }
 
+    checkValidity = (value, rules) => {
+        let isValid = false;
+
+        if (rules.required) {
+            isValid = value.trim() !== '';
+        }
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        console.log(inputIdentifier);
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
+        
+        this.setState({
+            orderForm: updatedOrderForm
+        });
+    }
+
     render() {
+        const formElementsArray = [];
+
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (                
-        <form>
-            <Input elementType="..." elementConfig="..." value="..." />
-            <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-            <Input inputtype="input" type="text" name="street" placeholder="Your Street" />
-            <Input inputtype="input" type="text" name="postal" placeholder="Your Postal Code" />
+        <form onSubmit={this.orderHandler}>
+            {formElementsArray.map(formElement => (
+                <Input 
+                    key={formElement.id}
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+            ))}
             <Button 
                 btnType="Success"
                 clicked={this.orderHandler}>ORDER</Button>
